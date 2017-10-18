2e8bf9e76169e0cbbbd3f4fce17178b68ad308b7
= <swk.kenyon@gmail.com>
2017-10-18 17:54:15 -0400

    change to databse configuration

--- vcombinator/vcombinator/settings.py
+++ vcombinator/vcombinator/settings.py
@@ -90,11 +90,11 @@ if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': 'vcombinator_backend',
+            'USER': 'vcapp',
+            'PASSWORD': 'Password1234',
+            'HOST': '',
+            'PORT': '',
         }
     }
 else:
