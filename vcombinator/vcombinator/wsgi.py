b9a18fd80b86a0ec8e22ed4be4017c0d3da6b20f
= <swk.kenyon@gmail.com>
2017-10-19 16:52:07 -0400

    test

--- vcombinator/vcombinator/wsgi.py
+++ vcombinator/vcombinator/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os, sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcombinator.settings")
 
 sys.path.append('/opt/python/current/app/')
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcombinator.settings")
 
 application = get_wsgi_application()
