9d36cbb30cebc1bbd6a0781f406440fbe8007a5d
= <swk.kenyon@gmail.com>
2017-10-19 16:30:52 -0400

    adding path

--- vcombinator/vcombinator/wsgi.py
+++ vcombinator/vcombinator/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+sys.path.append('/opt/python/current/app/')
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcombinator.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcombinator.settings")
 
 application = get_wsgi_application()
