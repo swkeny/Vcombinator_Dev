a9e951a0f37a77ccb95959189aa604a7fe91e246
= <swk.kenyon@gmail.com>
2017-10-19 21:11:09 -0400

    adding path

--- vcombinator/vcombinator/wsgi.py
+++ vcombinator/vcombinator/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os, sys
+print("Was I first?")
 sys.path.append('/opt/python/run/venv/lib/python3.4/site-packages')
 
 from django.core.wsgi import get_wsgi_application
