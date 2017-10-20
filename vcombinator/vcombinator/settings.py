4d66861787ae16e3582564f5631cba7ee8909946
= <swk.kenyon@gmail.com>
2017-10-20 15:06:27 -0400

    create superuser command

--- vcombinator/vcombinator/settings.py
+++ vcombinator/vcombinator/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vcombinator.urls'
+ROOT_URLCONF = 'vcombinator.vcombinator.urls'
 
 TEMPLATES = [
     {
