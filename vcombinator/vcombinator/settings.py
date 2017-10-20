2a18fdc6c5691ba07370cc25a33a20c2413b7042
= <swk.kenyon@gmail.com>
2017-10-20 10:34:08 -0400

    removing extra conf file

--- vcombinator/vcombinator/settings.py
+++ vcombinator/vcombinator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gvuogi+$v2qn88zpg4758lu99((3ay59$h(+8qz5zwrvv3%t_@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vcombinator.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['vcombinator.us-east-1.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
