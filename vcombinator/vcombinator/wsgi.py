49461fc0f604031e96acbf95fd58f344775b0538
= <swk.kenyon@gmail.com>
2017-10-19 21:05:23 -0400

    adding path

--- vcombinator/vcombinator/wsgi.py
+++ vcombinator/vcombinator/wsgi.py
@@ -13,7 +13,6 @@ sys.path.append('/opt/python/run/venv/lib/python3.4/site-packages')
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcombinator.settings")
 
-with open("/home/ec2-user/out.txt", "w") as f_out:
-    f_out.write("I was called!")
-    
+print("I was called!")
+
 application = get_wsgi_application()
