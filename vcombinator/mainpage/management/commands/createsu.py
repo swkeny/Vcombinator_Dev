4fb76c41098fbf298898283efdb87ebd42350287
= <swk.kenyon@gmail.com>
2017-10-20 14:56:03 -0400

    create superuser command

--- vcombinator/mainpage/management/commands/createsu.py
+++ vcombinator/mainpage/management/commands/createsu.py
@@ -5,5 +5,6 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("Test1", "example@example.com", "SummerOf17")
+        print("HELLOHELLOHELLOHELLOHELLO")
+        # if not User.objects.filter(username="admin").exists():
+        #     User.objects.create_superuser("Test1", "example@example.com", "SummerOf17")
