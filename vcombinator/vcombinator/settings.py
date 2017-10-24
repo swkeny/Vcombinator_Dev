f728b2ae4e84da4bada1881d7fbee69edeadd835
= <swk.kenyon@gmail.com>
2017-10-24 12:02:16 -0400

    Latest updates for s3 static content

--- vcombinator/vcombinator/settings.py
+++ vcombinator/vcombinator/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vcombinator.vcombinator.urls'
+ROOT_URLCONF = 'vcombinator.urls'
 
 TEMPLATES = [
     {
